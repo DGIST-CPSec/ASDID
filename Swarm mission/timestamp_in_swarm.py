2e617deb2c3d7614f9eac4ede6f395a795ad7214
KangJongHyun <129854502+KangJongHyun@users.noreply.github.com>
2023-08-28 18:28:02 +0900

    Create timestamp_in_swarm.py

---
+++ Swarm mission/timestamp_in_swarm.py
@@ -0,0 +1,231 @@
+import time
+import datetime
+import os
+import math
+import matplotlib.pyplot as plt
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.positioning.position_hl_commander import PositionHlCommander
+from cflib.crazyflie.log import LogConfig
+import csv
+
+# URI to the Crazyflie to connect to
+leader_uri = 'radio://0/80/2M/E7E7E7E704'
+follower_uri = 'radio://0/80/2M/E7E7E7E70C'
+
+follower_phlc = None
+follower_x, follower_y, follower_z = 0, 0, 0
+
+leader_positions = {'x': [], 'y': [], 'z': []}
+follower_positions = {'x': [], 'y': [], 'z': []}
+leader_orientations = {'roll': [], 'pitch': [], 'yaw': []}
+follower_orientations = {'roll': [], 'pitch': [], 'yaw': []}
+follower_receive_times = []
+
+def initialize(scf):
+    scf.cf.param.request_update_of_all_params()
+    scf.cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    scf.cf.param.set_value('kalman.resetEstimation', '0')
+    print('[INIT]: kalman prediction reset')
+    scf.cf.param.set_value('health.startPropTest', '1')  # propeller test before flight
+    time.sleep(5)
+    print('[INIT]: initialization complete')
+
+def position_callback(timestamp, data, logconf):
+    x = data['stateEstimate.x']
+    y = data['stateEstimate.y']
+    z = data['stateEstimate.z']
+    print('Position: ({}, {}, {})'.format(x, y, z))
+    roll = data['stateEstimate.roll']
+    pitch = data['stateEstimate.pitch']
+    yaw = data['stateEstimate.yaw']
+    
+    leader_orientations['roll'].append(roll)
+    leader_orientations['pitch'].append(pitch)
+    leader_orientations['yaw'].append(yaw)
+    follower_receive_times.append(time.time())
+    
+    leader_positions['x'].append(x)
+    leader_positions['y'].append(y)
+    leader_positions['z'].append(z)
+
+
+    # 팔로워 드론의 목표 위치를 리더 드론의 현재 위치로 설정
+    distance = math.sqrt((x - follower_x) ** 2 + (y - follower_y) ** 2 + (z - follower_z) ** 2) # 팔로워 드론과 리더 드론 사이의 거리를 계산합니다.
+
+    if distance < 0.3: # 드론들 사이의 거리가 0.3미터 미만일 경우
+        # 팔로워 드론이 리더 드론에서 멀어지도록 목표 위치를 설정합니다.
+        follower_phlc.go_to(follower_x - 0.1 if follower_x > x else follower_x + 0.1, follower_y - 0.1 if follower_y > y else follower_y + 0.1, follower_z)
+    else:
+        follower_phlc.go_to(x, y, z)
+
+
+def follower_position_callback(timestamp, data, logconf):
+    global follower_x, follower_y, follower_z
+    follower_x = data['stateEstimate.x']
+    follower_y = data['stateEstimate.y']
+    follower_z = data['stateEstimate.z']
+    
+    follower_positions['x'].append(follower_x)
+    follower_positions['y'].append(follower_y)
+    follower_positions['z'].append(follower_z)
+    
+    roll = data['stateEstimate.roll']
+    pitch = data['stateEstimate.pitch']
+    yaw = data['stateEstimate.yaw']
+
+    follower_orientations['roll'].append(roll)
+    follower_orientations['pitch'].append(pitch)
+    follower_orientations['yaw'].append(yaw)
+
+
+logconf = LogConfig(name='Position', period_in_ms=100)
+logconf.add_variable('stateEstimate.x', 'float')
+logconf.add_variable('stateEstimate.y', 'float')
+logconf.add_variable('stateEstimate.z', 'float')
+logconf.add_variable('stateEstimate.roll', 'float')   # 롤, 피치, 요 로그 변수 추가
+logconf.add_variable('stateEstimate.pitch', 'float')
+logconf.add_variable('stateEstimate.yaw', 'float')
+
+# Create separate LogConfig for follower drone
+follower_logconf = LogConfig(name='Follower Position', period_in_ms=100)
+follower_logconf.add_variable('stateEstimate.x', 'float')
+follower_logconf.add_variable('stateEstimate.y', 'float')
+follower_logconf.add_variable('stateEstimate.z', 'float')
+
+# Other parts of the code remain the same...
+
+
+
+def mission_phlc(leader_cf, follower_cf, code, leader_pos, follower_pos):
+    global follower_phlc
+    takeoff_height = 1.0
+
+    leader_phlc = PositionHlCommander(leader_cf, x=leader_pos[0], y=leader_pos[1], z=leader_pos[2])
+    follower_phlc = PositionHlCommander(follower_cf, x=follower_pos[0], y=follower_pos[1], z=follower_pos[2])
+    
+    leader_phlc.take_off(takeoff_height, 1.0)
+    follower_phlc.take_off(takeoff_height, 1.0)  # 팔로워 드론도 같이 이륙
+    time.sleep(5)
+    print('[MISSION]: takeoff complete')
+
+    leader_phlc.set_default_height(1.0)
+    follower_phlc.set_default_height(1.0)
+    leader_phlc.set_default_velocity(0.5)
+    follower_phlc.set_default_velocity(0.5)
+    leader_phlc.set_landing_height(0.0)
+    follower_phlc.set_landing_height(0.0)
+
+    if code == 1: # takeoff->up->down->land
+        leader_phlc.up(0.5)
+        time.sleep(4)
+        leader_phlc.down(0.5)
+        time.sleep(4)
+    elif code == 2: # linear round trip
+        leader_phlc.forward(1)
+        time.sleep(4)
+        leader_phlc.back(1)
+        time.sleep(4)
+    elif code == 3: # triangular round trip
+        leader_phlc.go_to(0.0, 1.2)
+        time.sleep(4)
+        leader_phlc.go_to(-1.2, 1.2)
+        time.sleep(4)
+        leader_phlc.go_to(0.0, 0.0)
+        time.sleep(4)
+    elif code == 4: # square round trip
+        leader_phlc.go_to(0.6, 0.6)
+        time.sleep(3)
+        leader_phlc.go_to(0.6, -0.6)
+        time.sleep(3)
+        leader_phlc.go_to(-0.6, -0.6)
+        time.sleep(3)
+        leader_phlc.go_to(-0.6, 0.6)
+        time.sleep(3)
+        leader_phlc.go_to(0.0, 0.0)
+        time.sleep(3)
+    elif code == 5: # circular round trip
+        with MotionCommander(leader_cf, default_height=takeoff_height) as leader_mc:
+            leader_mc.circle_left(0.5, 0.4)
+            time.sleep(10)
+    else:
+        time.sleep(5) # up-down
+
+    print('[MISSION]: mission command complete')
+    leader_phlc.land()
+    follower_phlc.land()
+    print('[MISSION]: landing')
+    
+def plot_positions():
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.plot(leader_positions['x'], leader_positions['y'], leader_positions['z'], '-o', label='Leader', color='blue')
+    ax.plot(follower_positions['x'], follower_positions['y'], follower_positions['z'], '-o', label='Follower', color='red')
+
+    ax.set_title('Drone Paths in 3D')
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_zlabel('Z Position (m)')
+    ax.legend()
+
+    plt.show()
+
+
+def save_to_csv(filename, data):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = list(data.keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for i in range(len(data['x'])):  # 'x'를 기준으로 loop를 돌립니다.
+            writer.writerow({k: data[k][i] for k in fieldnames})
+            
+def save_follower_receive_times_to_csv(filename, receive_times):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['시간 간격 (초)'])
+        
+        for i in range(1, len(receive_times)):
+            time_interval = receive_times[i] - receive_times[i - 1]
+            writer.writerow([time_interval])
+
+def main():
+    now = datetime.datetime.now()
+    missNo = int(input('Mission type:\n[0] Blink\t[1] In-position\t[2] Line\t[3] Triangle\t[4] Square\t[5] Circle\t[6] Up-down\n>> '))
+    leader_init_pos = list(map(int, input('Start position of leader drone (x, y, z in meters):\n>> ').split()))
+    follower_init_pos = list(map(int, input('Start position of follower drone (x, y, z in meters):\n>> ').split()))
+
+    cflib.crtp.init_drivers()
+
+    with SyncCrazyflie(leader_uri, cf=Crazyflie(rw_cache='./cache')) as leader_cf, \
+         SyncCrazyflie(follower_uri, cf=Crazyflie(rw_cache='./cache')) as follower_cf:
+        try:
+            initialize(leader_cf)
+            initialize(follower_cf)
+
+            leader_cf.cf.log.add_config(logconf)
+            logconf.data_received_cb.add_callback(position_callback)
+            logconf.start()
+            
+            follower_cf.cf.log.add_config(follower_logconf)
+            follower_logconf.data_received_cb.add_callback(follower_position_callback)
+            follower_logconf.start()
+            
+            mission_phlc(leader_cf, follower_cf, missNo, leader_init_pos, follower_init_pos)
+
+            print('[MAIN]: mission complete. Rebooting...')
+        except KeyboardInterrupt:
+            print('\n\n[MAIN]: EMERGENCY STOP TRIGGERED\n')
+
+if __name__ == '__main__':
+    main()
+    plot_positions()
+    save_to_csv("leader_positions.csv", leader_positions)
+    save_to_csv("follower_positions.csv", follower_positions)
+    save_to_csv("leader_orientations.csv", leader_orientations)
+    save_to_csv("follower_orientations.csv", follower_orientations)
+    save_follower_receive_times_to_csv("follower_receive_times.csv", follower_receive_times)
