22efe0c5b56d3e49f3138893168ba3faaaee19d5
KangJongHyun <129854502+KangJongHyun@users.noreply.github.com>
2024-01-19 16:01:15 +0900

    Update R2_Leader_Follower.py

--- Swarm mission/DEMO/R2_Leader_Follower.py
+++ Swarm mission/DEMO/R2_Leader_Follower.py
@@ -30,13 +30,13 @@ initialPos = [
 ]
 
 moveDelta = [
-    [1.0, 0.0, 0.0], # 각 드론을 앞으로 1미터 이동
-    [1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0],
-    [1.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0], # 각 드론을 앞으로 1미터 이동
+    [0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 1.0, 0.0],
 ]
 
 missNo    = int(input('Mission type: \n - [0]Blink \n - [1]In-position \n - [2]Line \n - [6]Up-down\n >>'))
@@ -84,31 +84,44 @@ def leader_position_callback(timestamp, data, logconf):
     prev_leader_x, prev_leader_y, prev_leader_z = leader_x, leader_y, leader_z
     
 # 팔로워 드론을 이동시키는 함수
-def move_follower_drone(uri, delta_x, delta_y, delta_z):
+def move_follower_drone(uri, takeoff_height):
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         phlc = PositionHlCommander(scf)
-        phlc.move_distance(delta_x, delta_y, delta_z)
+        phlc.take_off(takeoff_height, 2.0)
+
+
+def follower_sync_movement(scf: SyncCrazyflie, posNo):
+    # 팔로워 드론이 리더 드론의 움직임을 따라가도록 구현
+    # 예: 팔로워 드론의 현재 상태 확인, 리더 드론의 이동 대기, 동기화된 이동 등
+    pass
     
 def mission(scf: SyncCrazyflie, posNo, code):
     takeoff_height = 1.0
 
-    if code==2 and scf.cf.link_uri == leader_drone:  # 리더 드론일 경우에만 동작
+    # 리더 드론일 경우에만 동작
+    if code==2 and scf.cf.link_uri == leader_drone:
         phlc = PositionHlCommander(scf, 
                                    x=initialPos[posNo][0], 
                                    y=initialPos[posNo][1], 
                                    z=initialPos[posNo][2])
-        phlc.take_off(1.0, 1.0)
-        time.sleep(2)
+        phlc.take_off(takeoff_height, 2.0)
+        time.sleep(4)
 
         # 리더 드론을 정의된 대로 앞으로 1미터 이동
         phlc.move_distance(moveDelta[posNo][0], 
                            moveDelta[posNo][1], 
                            moveDelta[posNo][2])
-        time.sleep(2)
+        time.sleep(4)
+
+        # 리더 드론 이동 후 팔로워 드론 이륙 및 동기화
+        for follower_uri in follower_drones:
+            move_follower_drone(follower_uri, takeoff_height)
+            time.sleep(1)  # 팔로워 드론 간 이륙 간격
 
         phlc.land()
-
-leader_drone = None
+    else:
+        # 팔로워 드론은 리더 드론의 이륙을 기다린 후 동기화된 움직임을 수행
+        follower_sync_movement(scf, posNo)
 
 def main():
     global leader_drone, follower_drones
